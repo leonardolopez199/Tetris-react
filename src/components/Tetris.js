2cd6599de8d7671fbdf847d8e628ff7fd96d7f70
Leonardo Lopez <leonardolopez199@hotmail.com>
2022-06-29 17:49:29 +0100

    Drop piece"

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -8,6 +8,7 @@ import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 // Custom Hooks
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { useInterval } from '../hooks/useInterval';
 
 // Components
 import Stage from './Stage';
@@ -21,8 +22,6 @@ const Tetris = () => {
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
-  console.log('re-render');
-
   const movePlayer = dir => {
     if (!checkCollision(player, stage, { x: dir, y: 0 })) {
       updatePlayerPos({ x: dir, y: 0 });
@@ -30,9 +29,9 @@ const Tetris = () => {
   }
 
   const startGame = () => {
-    console.log("test")
     // Reset everything
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
     setGameOver(false);
   }
@@ -51,8 +50,13 @@ const Tetris = () => {
     }
   }
 
+  const keyUp=({keyCode}) =>{
+    if(!gameOver) if(keyCode === 40) setDropTime(1000);    
+  };
+
   const dropPlayer = () => {
-    drop();
+    setDropTime(null);
+    drop();    
   }
 
   const move = ({ keyCode }) => {
@@ -64,8 +68,12 @@ const Tetris = () => {
     }
   }
 
+  useInterval(() => {
+    drop();
+  }, dropTime);
+
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={ e => move(e) }>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={ e => move(e) } onKeyUp={keyUp}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
